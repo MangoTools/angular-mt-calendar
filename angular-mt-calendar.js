5f7f0853907df11823b8ccea334baf3ea2d67637
adidoo <adrien@mango.tools>
2014-10-13 17:43:47 +0200

    new directive name: mt-calendar

--- angular-mt-calendar.js
+++ angular-mt-calendar.js
@@ -10,7 +10,7 @@
 'use strict';
 
 angular.module('angular.mt.calendar', [])
-    .directive('angularMtCalendar', [ function() {
+    .directive('mtCalendar', [ function() {
         return {
             restrict: 'AE',
             scope: {
